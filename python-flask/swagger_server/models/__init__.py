898e33b70daa8a02784d215dabce57d4a133be6f
skoulouzis <S.Koulouzis@uva.nl>
2020-10-21 23:11:52 +0200

    delete old code

--- python-flask/swagger_server/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from swagger_server.models.node_template_map import NodeTemplateMap
-from swagger_server.models.node_template_map2 import NodeTemplateMap2
-from swagger_server.models.topology_template import TopologyTemplate
-from swagger_server.models.tosca_template import ToscaTemplate
