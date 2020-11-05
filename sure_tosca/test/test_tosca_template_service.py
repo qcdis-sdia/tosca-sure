e6926248b61535a0bb3f6b16894c3c68d28a334c
skoulouzis <S.Koulouzis@uva.nl>
2020-11-05 13:20:44 +0100

    fixed tests

--- sure_tosca/test/test_tosca_template_service.py
+++ sure_tosca/test/test_tosca_template_service.py
@@ -1,5 +1,8 @@
 import os
+import tempfile
 from unittest import TestCase
+
+import requests
 from six import BytesIO
 from werkzeug.datastructures import FileStorage
 import logging
@@ -17,19 +20,30 @@ logger.addHandler(h)
 logger.handler_set = True
 
 class Test(TestCase):
+    def download_file(self,url):
+        f = tempfile.NamedTemporaryFile(delete=False)
+        r = requests.get(url)
+        with open(f.name, 'wb') as f:
+            f.write(r.content)
+        return f.name
+
     def test_upload(self):
+        base_url = 'https://raw.githubusercontent.com/qcdis-sdia/sdia-tosca/master/examples/'
         file_names = ['application_example_outputs.yaml','application_example_2_topologies.yaml','lifeWatch_vre1.yaml',
                       'application_example_updated.yaml','compute.yaml','application_example_provisioned.yaml','topology.yaml',
                       'kubernetes.yaml','application_example_planed.yaml']
 
         for file_name in file_names:
+            path = self.download_file(base_url+file_name)
             logger.info("Testing : " + str(file_name))
-            doc_id = tosca_template_service.save(self.upload_file(file_name))
+            doc_id = tosca_template_service.save(self.upload_file(path))
             logger.info("doc_id : " + str(doc_id))
             self.assertTrue (doc_id >= 0)
 
     def test_get_tosca_template_model_by_id(self):
-        doc_id = tosca_template_service.save(self.upload_file('application_example_updated.yaml'))
+        base_url = 'https://raw.githubusercontent.com/qcdis-sdia/sdia-tosca/master/examples/'
+        path = self.download_file(base_url + 'application_example_updated.yaml')
+        doc_id = tosca_template_service.save(self.upload_file(path))
         tosca_template_dict = tosca_template_service.get_tosca_template_dict_by_id(doc_id)
         tosca_template_model = ToscaTemplateModel.from_dict(tosca_template_dict)
 
@@ -39,7 +53,9 @@ class Test(TestCase):
 
 
     def test_get_tosca_template_model_by_id(self):
-        doc_id = tosca_template_service.save(self.upload_file('application_example_updated.yaml'))
+        base_url = 'https://raw.githubusercontent.com/qcdis-sdia/sdia-tosca/master/examples/'
+        path = self.download_file(base_url + 'application_example_updated.yaml')
+        doc_id = tosca_template_service.save(self.upload_file(path))
         tosca_template_dict = tosca_template_service.get_tosca_template_dict_by_id(doc_id)
         tosca_template_model = ToscaTemplateModel.from_dict(tosca_template_dict)
 
@@ -47,12 +63,8 @@ class Test(TestCase):
         self.assertIsNotNone(tosca_template_model.topology_template)
         self.assertIsNotNone(tosca_template_model.topology_template.node_templates)
 
-    def upload_file(self, file_name):
-        tosca_path = "../../../TOSCA/"
-        input_tosca_file_path = tosca_path + '/' + file_name
-        if not os.path.exists(input_tosca_file_path):
-            tosca_path = "../TOSCA/"
-            input_tosca_file_path = tosca_path + '/' + file_name
+    def upload_file(self, path):
+        input_tosca_file_path = path
 
         dir_path = os.path.dirname(os.path.realpath(__file__))
         self.assertEqual(True, os.path.exists(input_tosca_file_path),
