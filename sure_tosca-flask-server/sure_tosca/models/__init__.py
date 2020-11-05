d139e414e106c0f7d801b8671af1ceffc21998f9
skoulouzis <S.Koulouzis@uva.nl>
2020-11-05 12:11:34 +0100

    restucture dir

--- sure_tosca-flask-server/sure_tosca/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from sure_tosca.models.node_template import NodeTemplateModel
-from sure_tosca.models.node_template_map import NodeTemplateMapModel
-from sure_tosca.models.topology_template import TopologyTemplateModel
-from sure_tosca.models.tosca_template import ToscaTemplateModel
